75aa532e0473ef470eff5e71dbcfec0bb1dce653
Raul Kuanyshev <ffkirito6@gmail.com>
2024-05-18 11:53:06 +0600

    map style

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 
-    <!-- <?php get_header(); ?> -->
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title></title>
-        <link rel="stylesheet"  href="node_modules/bootstrap/dist/css/bootstrap.min.css"  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="./index.css">
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-    </head>
-    <body>
+    <?php get_header(); ?>
+
+
+    
     <!-- main section start -->
 
     <section class="main position-relative mt-5 pb-5">
